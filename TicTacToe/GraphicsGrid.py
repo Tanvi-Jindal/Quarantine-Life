4c2de7637ad6d80bdd6be322a64df388fc448dd3
gohsumin <49374067+gohsumin@users.noreply.github.com>
2020-03-28 23:55:31 -0400

    updated display with tkinter

--- TicTacToe/GraphicsGrid.py
+++ TicTacToe/GraphicsGrid.py
@@ -1,22 +1,29 @@
-from graphics import GraphWin, Point, Rectangle, Line, Text
-#from Tkinter import *
+from Tkinter import *
 
 class GraphicsGrid():
 
     global window, sqrt, grid, text1
+    sqSize = 100
+    gapSize = 5
+    topSize = 50
+    bottomSize = 50
     
     def __init__(self):
-        window = GraphWin("TicTacToe", 320, 400)
-        sqr = Rectangle(Point(10, 50), Point(310, 350))
-        sqr.draw(window)
-        line1 = Line(Point(10, 150), Point(310, 150))
-        line2 = Line(Point(10, 250), Point(310, 250))
-        line3 = Line(Point(110, 50), Point(110, 350))
-        line4 = Line(Point(210, 50), Point(210, 350))
-        grid = [line1, line2, line3, line4]
-        for line in grid:
-            line.draw(window)
-        text1 = Text(Point(160, 25), 'Player: X   Computer: O\nPlayer\'s turn!')
+        master = Tk()
+        window = Canvas(master, width=self.sqSize*3+self.gapSize*4,
+            height=self.sqSize*3+self.gapSize*4+self.topSize+self.bottomSize)
+        window.pack()
+        sq1 = window.create_rectangle(self.gapSize, self.topSize+self.gapSize, self.sqSize+self.gapSize, self.topSize+self.sqSize+self.gapSize, fill="light blue", activefill="sky blue", outline="white")
+        sq2 = window.create_rectangle(self.sqSize+self.gapSize*2, self.topSize+self.gapSize, self.sqSize*2+self.gapSize*2, self.topSize+self.sqSize+self.gapSize, fill="light blue", activefill="sky blue", outline="white")
+        sq3 = window.create_rectangle(self.sqSize*2+self.gapSize*3, self.topSize+self.gapSize, self.sqSize*3+self.gapSize*3, self.topSize+self.sqSize+self.gapSize, fill="light blue", activefill="sky blue", outline="white")
+        sq4 = window.create_rectangle(self.gapSize, self.topSize+self.sqSize+self.gapSize*2, self.sqSize+self.gapSize, self.topSize+self.sqSize*2+self.gapSize*2, fill="light blue", activefill="sky blue", outline="white")
+        sq5 = window.create_rectangle(self.sqSize+self.gapSize*2, self.topSize+self.sqSize+self.gapSize*2, self.sqSize*2+self.gapSize*2, self.topSize+self.sqSize*2+self.gapSize*2, fill="light blue", activefill="sky blue", outline="white")
+        sq6 = window.create_rectangle(self.sqSize*2+self.gapSize*3, self.topSize+self.sqSize+self.gapSize*2, self.sqSize*3+self.gapSize*3, self.topSize+self.sqSize*2+self.gapSize*2, fill="light blue", activefill="sky blue", outline="white")
+        sq7 = window.create_rectangle(self.gapSize, self.topSize+self.sqSize*2+self.gapSize*3, self.sqSize+self.gapSize, self.topSize+self.sqSize*3+self.gapSize*3, fill="light blue", activefill="sky blue", outline="white")
+        sq8 = window.create_rectangle(self.sqSize+self.gapSize*2, self.topSize+self.sqSize*2+self.gapSize*3, self.sqSize*2+self.gapSize*2, self.topSize+self.sqSize*3+self.gapSize*3, fill="light blue", activefill="sky blue", outline="white")
+        sq9 = window.create_rectangle(self.sqSize*2+self.gapSize*3, self.topSize+self.sqSize*2+self.gapSize*3, self.sqSize*3+self.gapSize*3, self.topSize+self.sqSize*3+self.gapSize*3, fill="light blue", activefill="sky blue", outline="white")
+        mainloop()
+        """text1 = Text(Point(160, 25), 'Player: X   Computer: O\nPlayer\'s turn!')
         text1.draw(window)
         newButton = Rectangle(Point(70, 370), Point(150, 390))
         newButton.draw(window)
@@ -25,9 +32,7 @@ class GraphicsGrid():
         quitButton = Rectangle(Point(170, 370), Point(250, 390))
         quitButton.draw(window)
         quitText = Text(Point(210, 380), 'Quit')
-        quitText.draw(window)
-        window.getMouse()
-        window.close()
+        quitText.draw(window)"""\
 
     def updateDisplay(self):
         print("update")
